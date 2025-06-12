a39c09b8ee5e60c0c1fa715fe4513014ff1dfdd8
nang <you@example.com>
2025-06-12 14:00:01 +0700

    5 commit

--- config/filesystems.php
+++ config/filesystems.php
@@ -36,12 +36,12 @@ return [
             'report' => false,
         ],
 
-        'cloudinary' => [
-            'driver' => 'cloudinary',
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key' => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-        ],
+    'cloudinary' => [
+        'driver'     => 'cloudinary',
+        'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
+        'api_key'    => env('CLOUDINARY_API_KEY'),
+        'api_secret' => env('CLOUDINARY_API_SECRET'),
+    ],
 
     ],
 
