090cfc31ff5e5413bfcedebadeaaed69d1e40650
nang <you@example.com>
2025-06-13 11:34:08 +0700

    done

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'role' => 'seller',
+            'role' => 'admin',
             'profile_image' => $imageUrl,
             'profile_public_id' => $publicId,
         ]);
